ee25a30ecc1fb4253d380b76d33d519b291a640e
Benjamin Banwart <benjibanwart@gmail.com>
2022-06-18 16:05:17 -0500

    finished problems

---
+++ arrays/1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+my_array = np.array([1,2,3])
+
+def print_array(arr):
+    for i in arr:
+        print(i)
+
+#print_array(my_array)
--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,5 @@
+import numpy as np
 
+my_array = np.array([1,2,3])
+
+#print(np.multiply(my_array, 3))
